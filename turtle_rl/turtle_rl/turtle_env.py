27034b5ce8055c195d01d1c6962e7936850f8cee
Zixin <zixinye2026@u.northwestern.edu>
2026-02-14 22:35:27 -0600

    add laser reading sub

--- turtle_rl/turtle_rl/turtle_env.py
+++ turtle_rl/turtle_rl/turtle_env.py
@@ -1,5 +1,8 @@
 import rclpy
-from rclpy.node import Node 
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+
+import numpy as np
 
 class turtle_env(Node):
     """TurtleEnv class."""
@@ -7,6 +10,18 @@ class turtle_env(Node):
     def __init__(self):
         super().__init__('turtle_env')
         self.get_logger().info("The turtle_env node has just been created.")
+
+        # Subscription
+        self.laser_sub = self.create_subscription(LaserScan, 'scan', self.laser_callback, 1)
+
+        # Class variables
+        self._laser_readings = np.array(np.float32(10)*360) # LSD-02 can ony detect up to 8m, so 10m for inf
+
+    def laser_callback(self, msg: LaserScan):
+        self._laser_readings = np.array(msg.ranges)
+        # Convert infinity to 10m, since LSD-02 can only detect up to 8m
+        self._laser_readings[self._laser_readings == np.inf] =np.float32(10)
+
 
 def main(args=None):
     rclpy.init(args=args)
