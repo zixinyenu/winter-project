570e872738b7e7a50ca2770d44134791a8c5ae1a
Zixin <zixinye2026@u.northwestern.edu>
2026-02-16 22:33:10 -0600

    bridge gz services to ros and create gazebo env

--- turtle_rl/turtle_rl/gazebo_env.py
+++ turtle_rl/turtle_rl/gazebo_env.py
@@ -1,5 +1,7 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from ros_gz_interfaces.srv import ControlWorld, SetEntityPose, SpawnEntity, DeleteEntity
 
 import numpy as np
 
@@ -9,6 +11,33 @@ class gazebo_env(Node):
     def __init__(self):
         super().__init__('gazebo_env')
         self.get_logger().info("The gazebo_env node has just been created.")
+
+        # Client
+        self._callback_group = MutuallyExclusiveCallbackGroup()
+
+        self.control_world_cli = self.create_client(
+            ControlWorld,
+            '/world/empty/control',
+            callback_group=self._callback_group
+        )
+
+        self.set_entity_pose_cli = self.create_client(
+            SetEntityPose,
+            '/world/empty/set_pose',
+            callback_group=self._callback_group
+        )
+
+        self.spawn_entity_cli = self.create_client(
+            SpawnEntity,
+            '/world/empty/create',
+            callback_group=self._callback_group
+        )
+
+        self.delete_entity_cli = self.create_client(
+            DeleteEntity,
+            '/world/empty/remove',
+            callback_group=self._callback_group
+        )
 
 
 def main(args=None):
