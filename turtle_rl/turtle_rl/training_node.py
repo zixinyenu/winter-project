68443e536281ca1f4525a9402cf9dc6726b50c79
Zixin <zixinye2026@u.northwestern.edu>
2026-03-11 06:19:43 -0500

    phase 1.3 is generally good, total timesteps make more sense

--- turtle_rl/turtle_rl/training_node.py
+++ turtle_rl/turtle_rl/training_node.py
@@ -86,7 +86,7 @@ def main(args=None):
             #         model.save(f"{models_dir}/{algorithm}/{TIMESTEPS*i}")
             #         node.get_logger().info(f"Model {TIMESTEPS*i} has been saved")
 
-            TIMESTEPS = 500000
+            TIMESTEPS = 250000
             model.learn(
                 total_timesteps=TIMESTEPS,
                 reset_num_timesteps=False,
